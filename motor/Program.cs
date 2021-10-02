d0fc9d816bced35f5486feb7bc0cd64059330eea
w10 <w10@DESKTOP-I7GQVM8>
2021-10-01 21:48:55 -0500

    Agregar archivos de proyecto.

---
+++ motor/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Motor
+    {
+        private string tipo;
+        private double potencia;
+
+        public double Potencia
+        {
+            get
+            {
+                return Potencia;
+            }
+            set
+            {
+                potencia = value;
+            }
+        }
+
+
+        public string Tipo
+        {
+            get
+            {
+                return tipo;
+            }
+            set
+            {
+                tipo = value;
+            }
+        }
+        public Motor()
+        {
+            tipo = "";
+            potencia = 0.0;
+        }
+        public Motor(string tipo, double potencia)
+        {
+            this.tipo = tipo;
+            this.potencia = potencia;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "--> tipo:" + "potencias" + potencia;
+        }
+
+
+
+    
+}
+
+
+    
+
